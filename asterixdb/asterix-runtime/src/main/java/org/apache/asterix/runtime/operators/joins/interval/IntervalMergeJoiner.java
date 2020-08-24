0bdf2173e56d568f3268cf09cc61862f0e62e301
Caleb Herbel <caherbel@gmail.com>
2020-08-24 15:01:06 -0600

    Results changed

--- asterixdb/asterix-runtime/src/main/java/org/apache/asterix/runtime/operators/joins/interval/IntervalMergeJoiner.java
+++ asterixdb/asterix-runtime/src/main/java/org/apache/asterix/runtime/operators/joins/interval/IntervalMergeJoiner.java
@@ -233,6 +233,7 @@ public class IntervalMergeJoiner {
                 inputAccessor[PROBE_PARTITION], inputAccessor[PROBE_PARTITION].getTupleId())) {
             if (!addToMemory(inputAccessor[PROBE_PARTITION])) {
                 unfreezeAndClearMemory(writer, inputAccessor[BUILD_PARTITION]);
+                System.out.println("Spills");
                 return;
             }
         }
